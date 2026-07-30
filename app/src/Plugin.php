8e3dcbf9eb41cc4024f97ab73ed4eca4bcb71287
yamabiko <88073693+YamabikoLab@users.noreply.github.com>
2026-07-30 16:47:38 +0900

    Separate plugin runtime class from bootstrap

---
+++ app/src/Plugin.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Application composition for Yamabiko Blocks.
+ *
+ * @package YamabikoBlocks
+ */
+
+declare(strict_types=1);
+
+namespace YamabikoLab\Blocks;
+
+final class Plugin
+{
+    public static function init(): void
+    {
+        // Block registration will be added in a future issue.
+    }
+}
