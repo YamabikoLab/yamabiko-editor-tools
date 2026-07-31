2233743538019482a67e00e52b9da231dc664112
YamabikoLab <yamabiko.ocarina@gmail.com>
2026-08-01 07:05:38 +0900

    feat: add initial WordPress block structure and configuration files

---
+++ app/src/example/index.tsx
@@ -0,0 +1,26 @@
+import { useBlockProps } from '@wordpress/block-editor';
+import { registerBlockType } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
+
+import metadata from './block.json';
+
+function Edit() {
+	return (
+		<p { ...useBlockProps() }>
+			{ __( 'Yamabiko Blocks is ready.', 'yamabiko-blocks' ) }
+		</p>
+	);
+}
+
+function save() {
+	return (
+		<p { ...useBlockProps.save() }>
+			{ __( 'Yamabiko Blocks is ready.', 'yamabiko-blocks' ) }
+		</p>
+	);
+}
+
+registerBlockType( metadata.name, {
+	edit: Edit,
+	save,
+} );
