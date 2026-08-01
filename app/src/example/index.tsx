cd5805935b39ccda428c9d549c13cbceee110b47
YamabikoLab <yamabiko.ocarina@gmail.com>
2026-08-01 16:24:14 +0900

    chore: add Prettier configuration and format code in example block

--- app/src/example/index.tsx
+++ app/src/example/index.tsx
@@ -5,18 +5,12 @@ import { __ } from '@wordpress/i18n';
 import metadata from './block.json';
 
 function Edit() {
-	return (
-		<p { ...useBlockProps() }>
-			{ __( 'Yamabiko Blocks is ready.', 'yamabiko-blocks' ) }
-		</p>
-	);
+	return <p { ...useBlockProps() }>{ __( 'Yamabiko Blocks is ready.', 'yamabiko-blocks' ) }</p>;
 }
 
 function save() {
 	return (
-		<p { ...useBlockProps.save() }>
-			{ __( 'Yamabiko Blocks is ready.', 'yamabiko-blocks' ) }
-		</p>
+		<p { ...useBlockProps.save() }>{ __( 'Yamabiko Blocks is ready.', 'yamabiko-blocks' ) }</p>
 	);
 }
 
