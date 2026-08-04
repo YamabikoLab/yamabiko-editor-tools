70dfc66c3c1ad450d5efacab841feee98d21186c
yamabiko <88073693+YamabikoLab@users.noreply.github.com>
2026-08-04 15:19:19 +0900

    Add Table Reorder webpack entries

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const { getWebpackEntryPoints } = require( '@wordpress/scripts/utils/config' );
+
+module.exports = {
+	...defaultConfig,
+	entry: {
+		...getWebpackEntryPoints(),
+		'editor-extensions/table-reorder/index':
+			'./src/editor-extensions/table-reorder/index.tsx',
+		'editor-extensions/table-reorder/content':
+			'./src/editor-extensions/table-reorder/content.scss',
+	},
+};
