d2f3890d0d7c33d8f9a760c8311cb6c80a8a30fe
yamabiko <88073693+YamabikoLab@users.noreply.github.com>
2026-08-09 06:27:53 +0900

    Normalize npm SortableJS runtime export

--- src/editor-extensions/sortablejs-table-reorder-poc/content.ts
+++ src/editor-extensions/sortablejs-table-reorder-poc/content.ts
@@ -4,8 +4,29 @@ type SortableWindow = Window & {
 	Sortable?: typeof Sortable;
 };
 
-( window as SortableWindow ).Sortable = Sortable;
+type SortableImportShape = {
+	create?: unknown;
+	default?: typeof Sortable;
+};
+
+const sortableImport = Sortable as unknown as SortableImportShape;
+const sortableRuntime =
+	typeof sortableImport.create === 'function' ? Sortable : sortableImport.default;
+
+if ( sortableRuntime && typeof sortableRuntime.create === 'function' ) {
+	( window as SortableWindow ).Sortable = sortableRuntime;
+} else {
+	console.warn( '[Yamabiko SortableJS PoC] Sortable runtime shape is unsupported', {
+		createType: typeof sortableImport.create,
+		hasDefault: Boolean( sortableImport.default ),
+		importType: typeof Sortable,
+	} );
+}
 
 console.info( '[Yamabiko SortableJS PoC] Sortable runtime ready', {
+	available: Boolean( ( window as SortableWindow ).Sortable ),
+	createType: typeof ( window as SortableWindow ).Sortable?.create,
+	hasDefault: Boolean( sortableImport.default ),
+	importType: typeof Sortable,
 	inIframe: window !== window.parent,
 } );
