c817dac19eee985ed137ec019083ac8ada77ef79
yamabiko <88073693+YamabikoLab@users.noreply.github.com>
2026-08-13 02:02:52 +0900

    Include Table Reorder editor styles

--- src/editor-extensions/table-reorder/index.tsx
+++ src/editor-extensions/table-reorder/index.tsx
@@ -1,5 +1,6 @@
 import { addFilter } from '@wordpress/hooks';
 
+import './editor.scss';
 import { withTableReorder } from './with-table-reorder';
 
 addFilter( 'editor.BlockEdit', 'yamabiko-editor-tools/table-reorder', withTableReorder, 20 );
