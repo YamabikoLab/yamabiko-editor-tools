7bd925c2cc70f2e86d3880bc36c56a86a4880499
yamabiko <88073693+YamabikoLab@users.noreply.github.com>
2026-08-17 05:27:51 +0900

    Update controller test runtime loader mock

--- src/editor-extensions/table-reorder/controller/sortable-controller.test.ts
+++ src/editor-extensions/table-reorder/controller/sortable-controller.test.ts
@@ -4,13 +4,13 @@ import {
 	createTableContext,
 	type SortableRuntime,
 } from './sortable-controller.test-utils';
-import { ensureSortableRuntime } from './sortable-runtime';
+import { ensureSortableRuntime } from './sortable-runtime-loader';
 
 jest.mock( '@wordpress/components', () => ( {
 	Tooltip: ( { children }: { children: unknown } ) => children,
 } ) );
 
-jest.mock( './sortable-runtime', () => ( {
+jest.mock( './sortable-runtime-loader', () => ( {
 	ensureSortableRuntime: jest.fn(),
 } ) );
 
