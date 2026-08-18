c787300952a1fc955e2b5133e6448f69f38a813b
yamabiko <88073693+YamabikoLab@users.noreply.github.com>
2026-08-18 13:57:43 +0900

    Enforce Jest coverage threshold

--- jest-unit.config.js
+++ jest-unit.config.js
@@ -9,4 +9,12 @@ module.exports = {
 		'!src/types/**',
 		'!src/editor-extensions/table-reorder/index.tsx',
 	],
+	coverageThreshold: {
+		global: {
+			branches: 80,
+			functions: 80,
+			lines: 80,
+			statements: 80,
+		},
+	},
 };
